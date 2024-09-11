69b97a3cd8d8d2af9ba95db067d2d1e5deb21ef9
LAPTOP-J35KI0IE\admin <hungskygaren@gmail.com>
2024-09-11 21:24:55 +0700

    up

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,8 +11,8 @@ function Login() {
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <Row className="w-full max-w-[1440px] h-full max-h-[1024px] ">
-        <Col xs={{ span: 24 }} md={10} className="h-full">
-          <div className="relative bg-customGreen h-full flex flex-col  p-10 ">
+        <Col xs={{ span: 24 }} md={10} className="h-full w-full ">
+          <div className="overflow-hidden relative bg-customGreen h-full flex flex-col  p-10 ">
             <div className="absolute top-0 right-0 flex flex-row gap-2 ">
               <i
                 className="opacity-15 fa-solid fa-circle"
@@ -85,18 +85,18 @@ function Login() {
             </div>
 
             <Row>
-              <Col>
-                <div>
+              <Col xs={24} md={24}>
+                <div className="flex justify-center md:justify-start">
                   <img
                     src={logo}
                     alt="logo"
-                    className="w-full max-w-[100px] h-full rounded-3xl"
+                    className="w-1/3 max-w-[100px] h-auto rounded-3xl"
                   />
                 </div>
               </Col>
             </Row>
             <Row>
-              <Col xs={{ span: 0 }} span={24}>
+              <Col xs={{ span: 0 }} md={{ span: 24 }}>
                 <div className="mt-5 text-white">
                   <span className=" font-semibold text-3xl">
                     Đăng nhập <br />
@@ -110,41 +110,54 @@ function Login() {
                 </div>
               </Col>
               <Col xs={24} md={0}>
-                {" "}
-                <Form
-                  className="mt-4 w-full flex flex-col items-center"
-                  form={form}
-                  layout="vertical"
-                >
-                  <TextBox
-                    placeholder={"Nhập mã nhân viên"}
-                    classname={"w-29vw max-w-29vw"}
-                    classnameFI={"  flex  "}
-                    label={"Tên đăng nhập"}
-                    type={"text"}
-                  />
-                  <TextBox
-                    placeholder={"Nhập mật khẩu"}
-                    classname={"w-29vw max-w-29vw"}
-                    classnameFI={" flex   "}
-                    label={"Mật khẩu "}
-                    type="password"
-                  />
-                  <Form.Item className="  ">
-                    <Button
-                      className=" rounded-xl w-29vw max-w-29vw bg-customGreen text-white "
-                      htmlType="submit"
+                <div className="flex flex-col justify-center items-center">
+                  <div className="flex flex-col items-center text-slate-300">
+                    <h3 className="text-3xl font-bold text-center">
+                      Welcome back !{" "}
+                    </h3>
+                    <p className="mt-2 text-center">
+                      Giải pháp quản trị nâng cao trải nghiệm nhân sự đầu tiên
+                      tại Việt Nam
+                    </p>
+                  </div>
+                  <div className="bg-customGray rounded-xl mt-4 ">
+                    <Form
+                      className="p-5 flex flex-col items-center justify-between"
+                      form={form}
+                      layout="vertical"
                     >
-                      Đăng nhập
-                    </Button>
-                  </Form.Item>
-                  <Form.Item className=" ">
-                    <span className="">Bạn có nhớ mật khẩu ?</span>
-                    <Button type="link" className=" pl-1 text-customGreen">
-                      Quên mật khẩu
-                    </Button>
-                  </Form.Item>
-                </Form>
+                      <TextBox
+                        placeholder={"Nhập mã nhân viên"}
+                        classname={"w-64 h-10 "}
+                        classnameFI={" "}
+                        label={"Tên đăng nhập"}
+                        type={"text"}
+                      />
+
+                      <TextBox
+                        placeholder={"Nhập mật khẩu"}
+                        classname={" w-64 h-10"}
+                        classnameFI={"   "}
+                        label={"Mật khẩu "}
+                        type="password"
+                      />
+                      <Form.Item className="  ">
+                        <Button
+                          className=" rounded-2xl w-64 bg-customGreen text-white "
+                          htmlType="submit"
+                        >
+                          Đăng nhập
+                        </Button>
+                      </Form.Item>
+                      <Form.Item className=" ">
+                        <span className="">Bạn có nhớ mật khẩu ?</span>
+                        <Button type="link" className=" pl-1 text-customGreen">
+                          Quên mật khẩu
+                        </Button>
+                      </Form.Item>
+                    </Form>
+                  </div>
+                </div>
               </Col>
             </Row>
             <Row>
@@ -154,7 +167,7 @@ function Login() {
                     <div className="relative w-64 h-56">
                       <div className="absolute inset-0 rounded-xl border-2 border-slate-100 "></div>
                       <div className="absolute inset-2 rounded-lg overflow-hidden">
-                        <img src={imgg} alt="" className="w-full h-full " />
+                        <img src={imgg} alt="" className="w-full h-full  " />
                       </div>
                     </div>
                   </div>
@@ -165,7 +178,7 @@ function Login() {
                         <img
                           src={imgg}
                           alt=""
-                          className="w-full h-full  opacity-50"
+                          className="w-full h-full   opacity-50"
                         />
                       </div>
                     </div>
@@ -250,21 +263,21 @@ function Login() {
                   >
                     <TextBox
                       placeholder={"Nhập mã nhân viên"}
-                      classname={"w-29vw max-w-29vw"}
+                      classname={"w-96 max-w-29vw"}
                       classnameFI={"  flex  "}
                       label={"Tên đăng nhập"}
                       type={"text"}
                     />
                     <TextBox
                       placeholder={"Nhập mật khẩu"}
-                      classname={"w-29vw max-w-29vw"}
+                      classname={"w-96 max-w-29vw"}
                       classnameFI={" flex   "}
                       label={"Mật khẩu "}
                       type="password"
                     />
                     <Form.Item className="  ">
                       <Button
-                        className=" rounded-xl w-29vw max-w-29vw bg-customGreen text-white "
+                        className=" rounded-xl w-96 max-w-29vw bg-customGreen text-white "
                         htmlType="submit"
                       >
                         Đăng nhập
